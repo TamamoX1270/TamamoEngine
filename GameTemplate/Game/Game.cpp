4bddeee6c9558d87048dbcab7b9c1bc8d6e5c4d0
TamamoX1270 <sisyou1270@yahoo.co.jp>
2022-02-04 16:41:24 +0900

    できてたらいいな

--- GameTemplate/Game/Game.cpp
+++ GameTemplate/Game/Game.cpp
@@ -10,29 +10,19 @@ bool Game::Start()
 	//背景オブジェクトを作成する。
 	m_backGround = NewGO<BackGround>(0, "background");
 
-	//プレイヤーオブジェクトを作成する。
-//	m_player = NewGO<Player>(0, "player");
-
 	return true;
 }
 void Game::Update()
 {
-	//くぁｗせｄｒｆｔｇｙふじこｌｐ
-	//上田は寿司ブレード楽しい。
-	//たまご
 	//(*･ω･)/ﾊｰｲ
-	if (g_pad[0]->IsTrigger(enButtonDown)) {
-		m_player = NewGO<Player>(0, "player");
-	}
-
 
 	// 左スティック(キーボード：WASD)で平行移動。
 	m_pointligpos.x += g_pad[0]->GetLStickXF();
 	m_pointligpos.y += g_pad[0]->GetLStickYF();
-	g_directionLig.SetPointPos(m_pointligpos);
+//	g_directionLig.SetPointPos(m_pointligpos);
 	//ディレクションライトの当たる方向とカラーをセットできちゃいます
-	g_directionLig.SetLigDirection({ 1.0f,-1.0f,-1.0f });
-	g_directionLig.SetLigColor({0.5f,0.5f,0.5f});
+//	g_directionLig.SetLigDirection({ -1.0f,-1.0f,-1.0f });
+//	g_directionLig.SetLigColor({0.5f,0.5f,0.5f});
 }
 void Game::Render(RenderContext& rc)
 {
