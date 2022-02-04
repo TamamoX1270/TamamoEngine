05bb4d534167b30aff0fd4752688055446f3612e
TamamoX1270 <sisyou1270@yahoo.co.jp>
2022-02-04 16:50:38 +0900

    ポイントライト実装 gitあげれたらいいなーーー

--- GameTemplate/Game/Game.cpp
+++ GameTemplate/Game/Game.cpp
@@ -18,8 +18,9 @@ void Game::Update()
 
 	// 左スティック(キーボード：WASD)で平行移動。
 	m_pointligpos.x += g_pad[0]->GetLStickXF();
-	m_pointligpos.y += g_pad[0]->GetLStickYF();
-//	g_directionLig.SetPointPos(m_pointligpos);
+	m_pointligpos.z += g_pad[0]->GetLStickYF();
+	m_pointligpos.y = 50.0f;
+	g_directionLig.SetPointPos(m_pointligpos);
 	//ディレクションライトの当たる方向とカラーをセットできちゃいます
 //	g_directionLig.SetLigDirection({ -1.0f,-1.0f,-1.0f });
 //	g_directionLig.SetLigColor({0.5f,0.5f,0.5f});
