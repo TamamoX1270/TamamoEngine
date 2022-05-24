58a429541d4f14644fe4119c998b090a361a1fc1
kbc21b28 <kbc21b28@stu.kawahara.ac.jp>
2022-05-24 12:45:09 +0900

    5/24 12:44 長戸 醤油を取った時の音を調整

--- GameTemplate/Game/SoySauce.cpp
+++ GameTemplate/Game/SoySauce.cpp
@@ -116,6 +116,7 @@ void SoySauce::Move()
 		//効果音をつける。
 		SoundSource* Pse = NewGO<SoundSource>(14);
 		Pse->Init(14);
+		Pse->SetVolume(10.0f);
 		Pse->Play(false);
 
 		//醤油カウントを１増やす。
@@ -131,6 +132,7 @@ void SoySauce::Move()
 		//効果音をつける。
 		SoundSource* Pse = NewGO<SoundSource>(14);
 		Pse->Init(14);
+		Pse->SetVolume(10.0f);
 		Pse->Play(false);
 
 		//醤油カウントを１増やす。
@@ -146,6 +148,7 @@ void SoySauce::Move()
 		//効果音をつける。
 		SoundSource* Pse = NewGO<SoundSource>(14);
 		Pse->Init(14);
+		Pse->SetVolume(10.0f);
 		Pse->Play(false);
 
 		//醤油カウントを１増やす。
